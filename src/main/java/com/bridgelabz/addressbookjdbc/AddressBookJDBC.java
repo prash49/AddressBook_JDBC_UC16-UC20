b0f33ab163f286f5c7dbc7eaad760d9cfb8c4b0d
Prashanth <imprash4u@gmail.com>
2021-10-04 19:36:56 +0530

    Added: Retrieving all Entries from Database

--- src/main/java/com/bridgelabz/addressbookjdbc/AddressBookJDBC.java
+++ src/main/java/com/bridgelabz/addressbookjdbc/AddressBookJDBC.java
@@ -1,7 +1,32 @@
 package com.bridgelabz.addressbookjdbc;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookJDBC {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("\t Welcome to Address Book Using JDBC ");
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println(" Press\n 1 ->  Retrieve data\n 2 -> Exit");
+            switch (scanner.nextInt()) {
+                case 1:
+                    retrieveData();
+                    break;
+                case 2:
+                    exit = true;
+            }
+        }
+    }
+
+    private static void retrieveData() {
+        AddressBookRepo addressBookRepo = new AddressBookRepo();
+        List<Contacts> employeeInfoList = addressBookRepo.retrieveData();
+        for (Contacts employee : employeeInfoList
+        ) {
+            System.out.println(employee + "\n");
+        }
     }
 }
---
+++ src/main/java/com/bridgelabz/addressbookjdbc/AddressBookRepo.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.addressbookjdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookRepo {
+    Connection connection;
+
+    private Connection getConnection() {
+        String url = "jdbc:mysql://127.0.0.1:3306/AddressBookService";
+        String username = "root";
+        String password = "rootpassword";
+        Connection connection = null;
+        try {
+
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public List<Contacts> retrieveData() {
+        ResultSet resultSet = null;
+        List<Contacts> addressBookList = new ArrayList<Contacts>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select * from AddressBook";
+            resultSet = statement.executeQuery(sql);
+            int count = 0;
+            while (resultSet.next()) {
+                Contacts contactInfo = new Contacts();
+                contactInfo.setFirstName(resultSet.getString("firstName"));
+                contactInfo.setLastName(resultSet.getString("Lastname"));
+                contactInfo.setAddress(resultSet.getString("address"));
+                contactInfo.setCity(resultSet.getString("city"));
+                contactInfo.setState(resultSet.getString("state"));
+                contactInfo.setZip(resultSet.getInt("zip"));
+                contactInfo.setPhoneNumber(resultSet.getString("phoneNumber"));
+                contactInfo.setEmailId(resultSet.getString("email"));
+                contactInfo.setBookName(resultSet.getString("bookName"));
+                contactInfo.setContactType(resultSet.getString("contactType"));
+                contactInfo.setDateAdded(resultSet.getDate("Date_added").toLocalDate());
+
+                addressBookList.add(contactInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return addressBookList;
+
+    }
+}
